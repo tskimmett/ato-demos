f320f3bc345334081a41ff5edc871295011aea81
Taylor Kimmett <taylor@cognitoforms.com>
2019-04-10 16:33:17 -0400

    updates

--- 1-script-vs-module/my-module.old.js
+++
@@ -1,4 +0,0 @@
-Cognito.ready("MyModule", ["dependency1", "dependency2"], function() {
-	// dependency1 and dependency2 have already been executed
-	// our modules are coupled via assumptions
-});
---
+++ hello-sfc/src/components/hello.vue
@@ -0,0 +1,22 @@
+<template>
+  <p>{{ greeting }}! Today's date and time is {{ now }}</p>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      greeting: 'Hello from Vue',
+      now: new Date().toLocaleString()
+    }
+  }
+  // update every second
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
